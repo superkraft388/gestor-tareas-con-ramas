a5dd42d2ff934443934740d2219ccf20f8d3d4fb
superkraft388 <wislinkhonde@hotmail.com>
2025-11-04 05:07:33 -0600

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let tareas = [];
+
+app.get('/tareas', (req, res) => {
+  res.json(tareas);
+});
+
+app.post('/tareas', (req, res) => {
+  const nuevaTarea = req.body;
+  tareas.push(nuevaTarea);
+  res.status(201).json(nuevaTarea);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
